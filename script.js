3a73f474b6ceaca403a40d576142e35b07c680a6
Tony Wang <tony94042@gmail.com>
2022-10-24 16:16:08 +1100

    HTML and CSS coding completed. Started the JS portion

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Connected");
+
+const rand = function(){
+  const mix = Math.floor(Math.random() * randColor.length)
+  body = mix;
+}
+const randColor = ['red','orange','purple','pink']
+
+const body = document.body.style.backgroundColor = randColor[];
+const button = document.querySelector('.btn')
+
+button.addEventListener('click', rand);
